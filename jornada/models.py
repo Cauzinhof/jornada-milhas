55d02aa7777e0dbcba4051898b23b68294bf6714
Carlos Fernandes <carlos.f.junior@outlook.com>
2023-07-23 11:11:12 -0400

    🔍️ Ajustado o limite de caracteres do preço dos destinos

--- jornada/models.py
+++ jornada/models.py
@@ -18,7 +18,7 @@ class Destino(models.Model):
         return f'destinos/{instance.nome.replace(" ","_").replace("/","-")}/{filename}'
     
     nome = models.CharField(max_length=30, blank=False, null=False)
-    preco = models.CharField(max_length=10, blank=False, null=False)
+    preco = models.CharField(max_length=20, blank=False, null=False)
     foto = models.ImageField(blank=False, upload_to=upload_path)
 
     def __str__(self) -> str:
